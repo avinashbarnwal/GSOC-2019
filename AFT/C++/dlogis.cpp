98186f7e6364306db272ded40d26488828407335
Philip Cho <chohyu01@cs.washington.edu>
2019-07-10 03:15:20 -0700

    Remove main() from cpp files

--- AFT/C++/dlogis.cpp
+++ AFT/C++/dlogis.cpp
@@ -2,18 +2,6 @@
 #include <math.h>
 #define PI 3.14159
 using namespace std;
-double dlogis(double x, double mu, double sd);
-
-int main()
-{
-   double x    = 0;
-   double mu   = 0;
-   double sd   = 1;
-   double res;
-   res = dlogis(x,mu,sd);
-   return 0;
-
-}
 
 double dlogis(double x, double mu , double sd)
 {
--- AFT/C++/plogis.cpp
+++ AFT/C++/plogis.cpp
@@ -2,17 +2,6 @@
 #include <math.h>
 #define PI 3.14159
 using namespace std;
-double plogis(double x, double mu, double sd);
-int main()
-{
-   double x    = 0;
-   double mu   = 0;
-   double sd   = 1;
-   double res;
-   res = plogis(x,mu,sd);
-   return 0;
-
-}
 
 double plogis(double x, double mu , double sd)
 {
