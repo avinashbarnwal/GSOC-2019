cc15db9418dd3b9d6b32a0b400332b3f13d6010f
avinash_barnwal <avinashbarnwal123@gmail.com>
2019-07-12 16:39:45 -0400

    changes to header file

--- AFT/C++/aft.h
+++ AFT/C++/aft.h
@@ -1,4 +1,5 @@
-double dlogis(double x, double mu , double sd);
-double dnorm(double x, double mu , double sd);
-double plogis(double x, double mu , double sd);
-double pnorm(double x, double mu , double sd);
+extern "C" double dlogis(double x, double mu , double sd);
+extern "C" double dnorm(double x, double mu , double sd);
+extern "C" double plogis(double x, double mu , double sd);
+extern "C" double pnorm(double x, double mu , double sd);
+extern "C" double _grad_norm(double x, double mu, double sd);
