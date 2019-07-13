a2d49706df795bf10f07e25c7dd0c430f8314a07
avinash_barnwal <avinashbarnwal123@gmail.com>
2019-07-12 22:45:06 -0400

    changes with grad norm

---
+++ AFT/C++/.ipynb_checkpoints/aft-checkpoint.h
@@ -0,0 +1,5 @@
+extern "C" double dlogis(double x, double mu , double sd);
+extern "C" double dnorm(double x, double mu , double sd);
+extern "C" double plogis(double x, double mu , double sd);
+extern "C" double pnorm(double x, double mu , double sd);
+extern "C" double _grad_norm(double x, double mu, double sd);
