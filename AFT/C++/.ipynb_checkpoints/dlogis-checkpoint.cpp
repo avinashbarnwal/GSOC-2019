6c695dc0978e8ce53661de17a2283e9b73dc4944
avinash_barnwal <avinashbarnwal123@gmail.com>
2019-07-12 23:45:47 -0400

    changes in readme

--- AFT/C++/.ipynb_checkpoints/dlogis-checkpoint.cpp
+++ AFT/C++/.ipynb_checkpoints/dlogis-checkpoint.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <cmath>
 #define PI 3.14159
+#include "aft.h"
 
 extern "C" double dlogis(double x, double mu , double sd)
 {
-	double pdf;
+  double pdf;
   pdf = std::exp((x-mu)/sd)/(sd*std::pow((1+std::exp((x-mu)/sd)),2));
   return pdf;
 
