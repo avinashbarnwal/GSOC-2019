ace7bf568084fb040ccadc9c990b2c0310ca6c0d
Philip Cho <chohyu01@cs.washington.edu>
2019-07-23 15:04:04 -0700

    Fix C++ AFT

--- AFT/C++/hessian_right.cpp
+++ AFT/C++/hessian_right.cpp
@@ -11,15 +11,14 @@ extern "C" double hessian_right(double y_lower,double y_higher,double y_pred,dou
 	double grad;
 	double hess;
   char* given_dist =  "normal";
-  z    = (std::log(y_higher)-y_pred)/sigma;
+  z    = (std::log(y_lower)-y_pred)/sigma;
   if(strcmp(dist, given_dist) == 0){
   		pdf       = dnorm(z,0,1);
       cdf       = pnorm(z,0,1);
   		grad      = grad_norm(z,0,1);
-  }
-  else{
+  } else{
   		pdf  = dlogis(z,0,1);
-      cdf  = dlogis(z,0,1);
+      cdf  = plogis(z,0,1);
   		grad = grad_logis(z,0,1);
   }
 	hess = ((1-cdf)*grad + std::pow(pdf,2))/(std::pow(sigma,2)*std::pow(1-cdf,2));
